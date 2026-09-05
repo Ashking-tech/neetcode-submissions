5c61165c9cfab4fde5a93aa578ad3f8904268061
Anshul Gawai <Ashking21@proton.me>
2026-09-05 21:59:18 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+      int l = 0,r = s.size() - 1;
+
+      while(l < r){
+        if(s[l] != s[r]){
+            return isPal(s,l + 1,r) or isPal(s,l,r -1);
+        }
+        l++;
+        r--;
+      }
+      return true;
+       
+    }
+
+    private:
+        bool isPal(const string& s,int l,int r){
+            while(l < r){
+                if(s[l] != s[r]){
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+
+        }
+};
